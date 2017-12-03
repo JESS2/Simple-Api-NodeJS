8a43490ded90cd1df93c40852e2ca2a653e0687e
root <root@ip-172-31-11-95.ap-northeast-2.compute.internal>
2017-12-03 08:40:45 +0000

    commit

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const express = require('express'),
     mysql = require('mysql'),
     app = express(),
     bodyParser = require('body-parser'),
-    port = 1995,
+    port = 9212,
     connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: 'root',
-        port: 8889,
+        password: 'dusl8753',
+        port: 3306,
         database: 'STUDY'
     });
 
@@ -33,6 +33,7 @@ app.get('/', function (req, res) {
     console.log('CONNECTION GET');
 });
 
+//리스트 불러오기
 app.get('/user', function (req, res) {
     console.log('CONNECTION GET - /user');
 
@@ -47,6 +48,7 @@ app.get('/user', function (req, res) {
     });
 });
 
+//리스트 삽입
 app.post('/user', function (req, res) {
     console.log('CONNECTION POST - /user');
 
@@ -61,6 +63,7 @@ app.post('/user', function (req, res) {
     });
 });
 
+//리스트 삭제
 app.delete('/user', function (req, res) {
     console.log('CONNECTION DELETE - /user');
 
